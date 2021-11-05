2f3e82f8a3e4c2382fe355059887d077232d7d44
MIA1kl <mia01kln@gmail.com>
2021-11-06 00:57:46 +0600

    Toolbar, menu and icon were added

--- app/src/main/java/com/android/mvpapp/MainActivity.kt
+++ app/src/main/java/com/android/mvpapp/MainActivity.kt
@@ -2,13 +2,18 @@ package com.android.mvpapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title="Inventory store"
 
         val fab: View = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
@@ -16,6 +21,21 @@ class MainActivity : AppCompatActivity() {
                 .setAction("Action", null)
                 .show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> finish()
+            R.id.basket -> {
+                Toast.makeText(this,"Item deleted",Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
     }
 
 
