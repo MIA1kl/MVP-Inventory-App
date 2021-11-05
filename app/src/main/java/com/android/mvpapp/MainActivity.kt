8521752670cea04c5355a909a656f992f0686bb8
MIA1kl <mia01kln@gmail.com>
2021-11-06 00:09:30 +0600

    added floatingActionBar

---
+++ app/src/main/java/com/android/mvpapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.android.mvpapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fab: View = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
+    }
+
+
+}
